f633f9228b051b9d31a6f4dcbd3ef2c3de41d04f
Dilzo16 <chathuradilshanudawaththa@gmail.com>
2023-06-12 03:52:52 +0530

    building the getx controller

---
+++ lib/store_controller.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class StoreController extends GetxController{
+  final storeName='Thick Shake'.obs;
+  final flowerCount=0.obs;
+  final storeStatus=true.obs;
+  final flowerList=[].obs;
+  final reviews=<StoreReviews>[].obs;
+
+  final storeNameEditingController=TextEditingController();
+  final reviewEditingController=TextEditingController();
+  final followerController=TextEditingController();
+  final reviewNameController=TextEditingController();
+
+  updateStoreName(String name){
+    storeName(name);
+  }
+
+  updateFlowerCount(){
+    flowerCount(flowerCount.value+1);
+  }
+
+  void storeStatusOpen(bool isOpen){
+    storeStatus(isOpen);
+  }
+
+}
---
+++ lib/update_store_name.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:getx_tut/store_controller.dart';
+
+class UpdateStoreName extends StatelessWidget {
+   UpdateStoreName({super.key});
+  final storeController=Get.find<StoreController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
